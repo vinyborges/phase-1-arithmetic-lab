77510dcdd0c01cc3ce93d5aaa01db9a0513be8e6
vinyborges <viny.bdj@gmail.com>
2023-02-01 17:55:57 -0800

    commit tree ec1a91ef5a67c2bb4ce09909900cbdf9abff9892

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1*num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 =14;
+const num4 =5;
+let mod = num3 % num4;
+
+const max = Math.max(1,2,4,3,5,6,8,10,20);
+
+
